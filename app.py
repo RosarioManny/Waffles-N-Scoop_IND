83ba80dde89f007f92cd029cdbea27faff811423
RosarioManny <rosario.emm47@gmail.com>
2025-03-15 19:15:19 -0400

    Home and Nav

--- app.py
+++ app.py
@@ -5,4 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return render_template("Home.html")
+  return render_template("home.html")
+
+@app.route("/about")
+def about():
+  return render_template("about.html")
+@app.route("/cart")
+def cart():
+  return render_template("cart.html")
+
+@app.route("/profile")
+def profile():
+  return render_template("profile.html")
