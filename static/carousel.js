b60a9d68ad3d0f146a06cf5ded5a81b25f28f2c7
RosarioManny <rosario.emm47@gmail.com>
2025-03-18 11:21:24 -0400

    Buttons functional

--- static/carousel.js
+++ static/carousel.js
@@ -1,17 +1,18 @@
-const button = document.querySelectorAll("[data-carousel-button]")
+const buttons = document.querySelectorAll("[data-carousel-button]")
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
     const offset = button.dataset.carouselButton === "next" ? 1 : -1
     const slides = button
-      .closest("[data-carousle]")
+      .closest("[data-carousel]")
       .querySelector("[data-slides]")
-    const activeSlide = slides.querySelector("[data-active]")
-    let newIdx = [...slides.children].indexOf(activeSLide) + offset
-    if (newIdx < 0) newIdx = slides.children.lenght - 1
-    if (newIdx >= slides.children.length) newIdx = 0
 
-    slides.children[newIdx].dataset.active = true
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
     delete activeSlide.dataset.active
   })
 })
