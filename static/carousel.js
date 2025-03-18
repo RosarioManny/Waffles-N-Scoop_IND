5c999b126fcabd402d26e4187d311a6a3143c5a2
RosarioManny <rosario.emm47@gmail.com>
2025-03-18 12:16:33 -0400

    Carousel Complete

--- static/carousel.js
+++ static/carousel.js
@@ -1,18 +1,35 @@
-const buttons = document.querySelectorAll("[data-carousel-button]")
+document.addEventListener("DOMContentLoaded",() => {
+  // Grabs elements
+  const carousel = document.querySelector("[data-carousel");
+  const slides = document.querySelector("[data-slides");
 
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1
-    const slides = button
-      .closest("[data-carousel]")
-      .querySelector("[data-slides]")
+  // Keep track of index
+  let currentIdx = 0;
+  // Track scrolling position
+  let isScrollingRight = true;
 
-    const activeSlide = slides.querySelector("[data-active]")
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    if (newIndex < 0) newIndex = slides.children.length - 1
-    if (newIndex >= slides.children.length) newIndex = 0
+  // Move carousel
+  const moveCarousel = () => {
+    const totalSlides = slides.children.length;
 
-    slides.children[newIndex].dataset.active = true
-    delete activeSlide.dataset.active
-  })
+    if(isScrollingRight) {
+      // Move by 1
+      currentIdx++;
+      // If at middle of list reverse
+      if (currentIdx >= totalSlides - 3) {
+        isScrollingRight = false;
+      }
+    } else  {
+      // Move backwards
+      currentIdx--;
+      // If reach the first image, stop and reverse
+      if (currentIdx < 0) {
+        currentIdx = 0;
+        isScrollingRight = true
+      }
+    }
+    
+    slides.style.transform = `translateX(-${currentIdx * 33.33}%)`;
+  }
+  setInterval(moveCarousel, 2700);
 })
