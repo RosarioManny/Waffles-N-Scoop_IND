fa6321ef09fb2d9eb54545454a4cc7170fbad5d1
RosarioManny <rosario.emm47@gmail.com>
2025-03-21 16:57:03 -0400

    Login required, History, Profile Pages

--- app.py
+++ app.py
@@ -1,26 +1,46 @@
-from flask import Flask, render_template, redirect, url_for
-
+from flask import Flask, render_template, redirect, url_for, request, session
+from flask_session import Session
+from helpers import login_required
 
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+# Session(id(app))
+
+# Home
 @app.route("/")
 def hello_world():
   return render_template("home.html")
 
+# Login
 @app.route("/login")
 def login():
   return render_template("login.html")
 
+# Register
 @app.route("/register")
 def register():
   return render_template("register.html")
+
+# About
 @app.route("/about")
 def about():
   return render_template("about.html")
+
+# Cart
 @app.route("/cart")
 def cart():
   return render_template("cart.html")
 
+# Profile
 @app.route("/profile")
+@login_required
 def profile():
   return render_template("profile.html")
+
+# History
+@app.route("/history")
+@login_required
+def history():
+  return render_template("history.html")
